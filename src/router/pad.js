2eb946abd154d8e29ae542e70a11ea7cc286ea1a
Victor Lucas <victorlucas@alu.ufc.br>
2018-08-30 10:05:06 -0300

    Correções e mais correções

--- src/router/pad.js
+++ src/router/pad.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+let router = express.Router(); // Criação de rota da nota
 
-let router = express.Router(); // criação de rota da nota
+const PROCESS = 'prod';
+const CLOUD =  (PROCESS === 'prod') ? '../Dontpad/' : './';
+
 
 router.get('/', (req,res) => {
-    res.sendFile(path.resolve('./src/html/inicio.html')); 
+    res.sendFile(path.resolve(`${CLOUD}src/html/inicio.html`)); 
 })
 
 router.get('/:pad', (req,res) => {
-    res.sendFile(path.resolve('./src/html/pad.html'));
+    res.sendFile(path.resolve(`${CLOUD}src/html/pad.html`));
 })
 
 router.post('/send/:pad', (req,res) => {
@@ -19,7 +22,7 @@ router.post('/send/:pad', (req,res) => {
 
     json = JSON.stringify(json);
 
-    fs.writeFile('./src/pads/'+req.params.pad+'.json', json, (err) => {
+    fs.writeFile(`${CLOUD}src/pads/${req.params.pad}.json`, json, (err) => {
         if (!err) {
             res.send('deubom');
         }
@@ -28,9 +31,9 @@ router.post('/send/:pad', (req,res) => {
 
 router.get('/recovery/:pad', (req,res) => {
     try {
-        res.send(JSON.parse(fs.readFileSync('./src/pads/'+req.params.pad+'.json', 'utf8')));
+        res.send(JSON.parse(fs.readFileSync(`${CLOUD}src/pads/${req.params.pad}.json`, 'utf8')));
     } catch(e) {
-        fs.writeFile('./src/pads/'+req.params.pad+'.json', JSON.stringify({ text: ''}), (err) => {
+        fs.writeFile(`${CLOUD}src/pads/${req.params.pad}.json`, JSON.stringify({ text: ''}), (err) => {
             if (!err) {
                 res.send('deubom');
             }
